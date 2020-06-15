c5d24d25fd56a30d71cc9d7b6e38c9342fb82543
Herminio Torres <herminiocesar@gmail.com>
2020-06-15 11:53:17 -0300

    Pig Latin

--- elixir/pig-latin/lib/pig_latin.ex
+++ elixir/pig-latin/lib/pig_latin.ex
@@ -15,21 +15,50 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase |> String.split() |> translate([]) |> Enum.reverse() |> Enum.join(" ")
+  end
+
+  defp translate([], accumulator), do: accumulator
+  defp translate([word], accumulator), do: translate([], [switch_letter(word) | accumulator])
+
+  defp translate([word | phrase], accumulator) do
+    translate(phrase, [switch_letter(word) | accumulator])
+  end
+
+  defp switch_letter(<<"ch", rest_word::binary>>), do: switch_letter("#{rest_word}ch")
+  defp switch_letter(<<"qu", rest_word::binary>>), do: switch_letter("#{rest_word}qu")
+  defp switch_letter(<<"squ", rest_word::binary>>), do: switch_letter("#{rest_word}squ")
+  defp switch_letter(<<"th", rest_word::binary>>), do: switch_letter("#{rest_word}th")
+  defp switch_letter(<<"thr", rest_word::binary>>), do: switch_letter("#{rest_word}thr")
+  defp switch_letter(<<"sch", rest_word::binary>>), do: switch_letter("#{rest_word}sch")
+
+  defp switch_letter(<<"x", second_letter::binary-size(1), rest_word::binary>>) do
     cond do
-      Regex.match?(~r/^(a|e|i|o|u|yt|xr)/, phrase) ->
-        end_with(phrase)
-      not Regex.match?(~r/^(a|e|i|o|u|yt|xr)/, phrase) ->
-        phrase
-        |> String.splitter("", trim: true)
-        |> Enum.to_list
-        |> reverse
-        |> List.to_string
-        |> end_with
-      true -> phrase
+      !Enum.member?(["a", "e", "i", "o", "u"], second_letter) ->
+        "x#{second_letter}#{rest_word}ay"
+
+      true ->
+        switch_letter("#{second_letter}#{rest_word}x")
     end
   end
 
-  defp reverse([head | tail]), do: tail ++ head
+  defp switch_letter(<<"y", second_letter::binary-size(1), rest_word::binary>>) do
+    cond do
+      !Enum.member?(["a", "e", "i", "o", "u"], second_letter) ->
+        "y#{second_letter}#{rest_word}ay"
 
-  defp end_with(phrase), do: phrase <> "ay"
+      true ->
+        switch_letter("#{second_letter}#{rest_word}y")
+    end
+  end
+
+  defp switch_letter(<<first_letter::binary-size(1), rest_word::binary>>) do
+    cond do
+      Enum.member?(["a", "e", "i", "o", "u"], first_letter) ->
+        "#{first_letter}#{rest_word}ay"
+
+      true ->
+        switch_letter("#{rest_word}#{first_letter}")
+    end
+  end
 end
