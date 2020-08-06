a84ba4a33d453ca4d3856afca3fe230b87301d78
Herminio Torres <herminiocesar@gmail.com>
2020-08-06 20:28:38 -0300

    add space_age from elixir track

--- elixir/space-age/lib/space_age.ex
+++ elixir/space-age/lib/space_age.ex
@@ -1,4 +1,16 @@
 defmodule SpaceAge do
+  @earth 31_557_600
+
+  @orbital_period [
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  ]
+
   @type planet ::
           :mercury
           | :venus
@@ -15,5 +27,9 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    case planet do
+      :earth -> (seconds / @earth)
+      _ -> ((seconds / @earth) / @orbital_period[planet])
+    end
   end
 end
