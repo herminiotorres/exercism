c9b20eb02c435d2c23e0ff3f6b13da46e2d4396e
Herminio Torres <herminiocesar@gmail.com>
2020-08-29 11:28:57 -0300

    refactor beer-song from elixir track

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,41 +3,39 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
   def verse(number) do
     """
-    #{bottle(number, true)} of beer on the wall, #{bottle(number, false)} of beer.
-    #{take_and_pass(number)}, #{number |> decrement |> bottle(false)} of beer on the wall.
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number-1} bottles of beer on the wall.
     """
-  end
-
-  defp take_and_pass(number) do
-    cond do
-      number > 1  -> "Take one down and pass it around"
-      number == 1 -> "Take it down and pass it around"
-      true        -> "Go to the store and buy some more"
-    end
-  end
-
-  defp decrement(number), do: number - 1
-
-  defp bottle(0, true = _beginning_phrase), do: "No more bottles"
-  defp bottle(0, false = _beginning_phrase), do: "no more bottles"
-  defp bottle(number, _beginning_phrase) do
-    cond do
-      number > 1  -> "#{number} bottles"
-      number == 1 -> "1 bottle"
-      true        -> "99 bottles"
-    end
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
-  def lyrics(), do: lyrics(99..0)
-  def lyrics(range) do
+  def lyrics(range \\ 99..0) do
     range
-    |> Enum.map(&verse/1)
-    |> Enum.join("\n")
+    |> Enum.map_join("\n", &verse/1)
   end
 end
