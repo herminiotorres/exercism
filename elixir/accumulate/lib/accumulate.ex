c7c965fbeb2c611077df2c402547bef752556049
Herminio Torres <herminiocesar@gmail.com>
2020-09-03 09:27:39 -0300

    refactor accumulate from elixir track

--- elixir/accumulate/lib/accumulate.ex
+++ elixir/accumulate/lib/accumulate.ex
@@ -17,16 +17,6 @@ defmodule Accumulate do
 
   @spec accumulate(list, (any -> any)) :: list
   def accumulate(list, fun) do
-    accumulate(list, fun, [])
-  end
-
-  defp accumulate([], _fun, acc), do: acc |> reverse([])
-  defp accumulate([head | tail], fun, acc) do
-    accumulate(tail, fun, [fun.(head) | acc])
-  end
-
-  defp reverse([], acc), do: acc
-  defp reverse([head | tail], acc) do
-    reverse(tail, [head | acc])
+    for item <- list, do: fun.(item)
   end
 end
