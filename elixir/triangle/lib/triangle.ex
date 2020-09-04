165bfc4ec0d5c1c91b551059ab1363d381f27242
Herminio Torres <herminiocesar@gmail.com>
2020-09-04 13:59:26 -0300

    refactor triangle from elixir track

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -1,23 +1,19 @@
 defmodule Triangle do
   @type kind :: :equilateral | :isosceles | :scalene
 
+  defguard is_negative(a \\ 0, b \\ 0, c \\ 0) when a <= 0 or b <= 0 or c <= 0
+  defguard is_inequality(a, b) when a >= b
+
   @doc """
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
-  def kind(a, a, a) when a <= 0, do: {:error, "all side lengths must be positive"}
+  def kind(a, b, c) when is_negative(a, b, c), do: {:error, "all side lengths must be positive"}
+  def kind(a, b, c) when is_inequality(b, a+c) or is_inequality(a, b+c) or is_inequality(c, a+b), do: {:error, "side lengths violate triangle inequality"}
+
   def kind(a, a, a), do: {:ok, :equilateral}
-
-  def kind(a, b, a) when a+a <= b, do: {:error, "side lengths violate triangle inequality"}
-  def kind(b, a, a) when a+a <= b, do: {:error, "side lengths violate triangle inequality"}
-  def kind(a, a, b) when a+a <= b, do: {:error, "side lengths violate triangle inequality"}
-  def kind(a, _b, a), do: {:ok, :isosceles}
-  def kind(_b, a, a), do: {:ok, :isosceles}
-  def kind(a, a, _b), do: {:ok, :isosceles}
-
-  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0, do: {:error, "all side lengths must be positive"}
-  def kind(a, b, c) when a+b <= c, do: {:error, "side lengths violate triangle inequality"}
-  def kind(a, b, c) when a+c <= b, do: {:error, "side lengths violate triangle inequality"}
-  def kind(a, b, c) when b+c <= a, do: {:error, "side lengths violate triangle inequality"}
-  def kind(_a, _b, _c), do: {:ok, :scalene}
+  def kind(a, a, _), do: {:ok, :isosceles}
+  def kind(a, _, a), do: {:ok, :isosceles}
+  def kind(_, a, a), do: {:ok, :isosceles}
+  def kind(a, b, c), do: {:ok, :scalene}
 end
