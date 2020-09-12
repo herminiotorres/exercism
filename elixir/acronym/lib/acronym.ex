2adcd4f3f00ae42bb563beb4d7e4d1853680a859
Herminio Torres <herminiocesar@gmail.com>
2020-09-11 21:21:57 -0300

    refactor acronym from elixir track

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -6,22 +6,9 @@ defmodule Acronym do
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
     string
-    |> String.split(~r/[\-[:blank:]]/)
-    |> Enum.map_join(&letters/1)
+    |> String.replace(~r/[[:punct:]]/, "")
+    |> (&(Regex.scan(~r/\b[[:alpha:]]|[[:upper:]](?=[[:lower:]])/, &1))).()
+    |> List.to_string
     |> String.upcase
-  end
-
-  defp letters(word) do
-    characters =
-      Regex.scan(~r/[[:upper:]]/, word)
-      |> Enum.map_join(&List.flatten/1)
-
-    cond do
-      characters != word and characters != "" ->
-        characters
-
-      true ->
-        word |> String.first
-    end
   end
 end
