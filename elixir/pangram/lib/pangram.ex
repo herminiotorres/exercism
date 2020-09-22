65fee64ac74bfcab8cd315d7facb403543df2fe0
Herminio Torres <herminiocesar@gmail.com>
2020-09-22 15:49:43 -0300

    refactor pangram from elixir track

--- elixir/pangram/lib/pangram.ex
+++ elixir/pangram/lib/pangram.ex
@@ -12,28 +12,15 @@ defmodule Pangram do
 
   """
 
-  @digits Enum.map(0..9, &Integer.to_string(&1))
+  @alphabet ?a..?z
 
   @spec pangram?(String.t()) :: boolean
   def pangram?(sentence) do
-    lower_or_mixed_case_with_punctuation?(sentence) &&
-    sentence_with_x_character?(sentence) &&
-    word_with_numbers?(sentence)
-  end
+    chartlist_sentence =
+      sentence
+      |> String.downcase
+      |> to_charlist
 
-  defp lower_or_mixed_case_with_punctuation?(sentence) do
-    String.match?(sentence, ~r/^[[:upper:]]{1}.|[[:lower:]]$/)
-  end
-
-  defp sentence_with_x_character?(sentence) do
-    String.match?(sentence, ~r/x/)
-  end
-
-  defp word_with_numbers?(sentence) do
-    sentence
-    |> String.split()
-    |> Enum.all?(fn word ->
-      word in @digits || not String.contains?(word, @digits)
-    end)
+    Enum.all?(@alphabet, & &1 in chartlist_sentence)
   end
 end
