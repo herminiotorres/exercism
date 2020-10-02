5933e4eadf2ae65252209938ba87c82bad220d26
Herminio Torres <herminiocesar@gmail.com>
2020-10-01 23:53:08 -0300

    refactor prime factors from elixir track

--- elixir/prime-factors/lib/prime_factors.ex
+++ elixir/prime-factors/lib/prime_factors.ex
@@ -12,12 +12,12 @@ defmodule PrimeFactors do
     calc(number, 2, [])
   end
 
-  defp calc(number, factor, acc) when number < factor, do: Enum.sort(acc)
-  defp calc(number, factor, acc) when div(number, factor) >= 1 and rem(number, factor) == 0 do
-    case acc |> Enum.filter(& rem(factor, &1) == 0 and &1 != factor) do
-      [] -> calc(div(number, factor), factor, [factor | acc])
-      _  -> calc(number, factor+1, acc)
-    end
+  defp calc(1, _, acc), do: Enum.reverse(acc)
+  defp calc(number, factor, acc) when number < factor * factor do
+    Enum.reverse([number | acc])
   end
+  defp calc(number, factor, acc) when rem(number, factor) == 0 do
+     calc(div(number, factor), factor, [factor | acc])
+   end
   defp calc(number, factor, acc), do: calc(number, factor+1, acc)
 end
